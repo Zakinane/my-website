04a9ed271ff32fe3fae96731979d040d58dea854
Zak <djabazaki@gmail.com>
2025-04-23 13:58:42 +0100

    style

--- JS/welcome.js
+++ JS/welcome.js
@@ -1,8 +1,34 @@
+//////////////////////////////////////////////////// Waving
 let welcome = document.getElementById('welcome')
-let waving = document.getElementById('waving')
+// let waving = document.getElementById('waving')
 
+
+
+
+/////////////////////////////////////////////////// Annoying
+var clicks = 0
 let annoying = false
 
 welcome.addEventListener('click', () => {
-
+    clicks++
+    console.log("hey")
+    if (clicks === 10)
+        annoying = true
+       console.log("nullos")
+   
+    setTimeout(() => {
+       clicks = 0
+   }, 5000);
+   
 })
+
+if(annoying === true){
+    whatYouDeserve()    
+}
+
+function whatYouDeserve(){
+    
+}
+
+ 
+
